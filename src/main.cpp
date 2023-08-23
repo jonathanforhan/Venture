a57a00b2f633a389df1e017a25571196b5450257
jonathanforhan <jonathan.forhan@gmail.com>
2023-08-23 17:31:52 -0400

    add external dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <ranges>
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hi\n";
-    return 0;
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+
+    vk::Instance instance;
+    uint32_t extensionCount = 0;
+    vk::Result result = vk::enumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    assert(result == vk::Result::eSuccess);
+    std::cout << "Extension Count: " << extensionCount << std::endl;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
